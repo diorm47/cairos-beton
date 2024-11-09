34f5ec3f31f0cf8eb45d182a15f8a8df81785173
diorm47 <dior47m@gmail.com>
2024-11-09 11:30:09 +0500

    nav drops

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
+document.querySelectorAll(".catalog_btn").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active_catalog_btn");
+
+    document.querySelectorAll(".catalog_btn").forEach((faq) => {
+      if (faq !== item) {
+        faq.classList.remove("active_catalog_btn");
+      }
+    });
+  });
+});
+
+document.querySelectorAll(".nav_menu_item").forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active_nav_menu_item");
+
+    document.querySelectorAll(".nav_menu_item").forEach((faq) => {
+      if (faq !== item) {
+        faq.classList.remove("active_nav_menu_item");
+      }
+    });
+  });
+});
+
+
+
+
 document.querySelectorAll(".faq_item").forEach((item) => {
   item.addEventListener("click", () => {
     item.classList.toggle("faq_item_active");
